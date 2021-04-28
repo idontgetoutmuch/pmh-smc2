c0ae92e26ee0e520efd26e6e3d3a4a403b7a0e77
Dominic Steinitz <dominic@steinitz.org>
2021-04-28 16:19:11 +0100

    Start of smc2 in Julia

--- shell.nix
+++ shell.nix
@@ -1,17 +1,52 @@
-{ pkgs ? import <nixpkgs> { config.allowBroken = true; } }:
+let
+
+nixpkgs = /Users/dom/nixpkgs;
+
+in
+
+{ pkgs ? import nixpkgs { config.allowBroken = true; } }:
 
 let
 
-  myJulia = callPackage ./. {};
+  # The base Julia version
+  baseJulia = pkgs.julia_15;
+
+  # Extra libraries for Julia's LD_LIBRARY_PATH.
+  # Recent Julia packages that use Artifacts.toml to specify their dependencies
+  # shouldn't need this.
+  # But if a package implicitly depends on some library being present, you can
+  # add it here.
+  extraLibs = [];
+
+  # Wrapped Julia with libraries and environment variables.
+  # Note: setting The PYTHON environment variable is recommended to prevent packages
+  # from trying to obtain their own with Conda.
+  julia = pkgs.runCommand "julia-wrapped" { buildInputs = [pkgs.makeWrapper]; } ''
+    mkdir -p $out/bin
+    makeWrapper ${baseJulia}/bin/julia $out/bin/julia \
+                --suffix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath extraLibs}" \
+                --set PYTHON ${pkgs.python3}/bin/python
+  '';
+
+myJulia = pkgs.callPackage ./common.nix {
+  inherit julia;
+
+  # Run Pkg.precompile() to precompile all packages?
+  precompile = true;
+
+  # Extra arguments to makeWrapper when creating the final Julia wrapper.
+  # By default, it will just put the new depot at the end of JULIA_DEPOT_PATH.
+  # You can add additional flags here.
+  makeWrapperArgs = "";
+};
 
 in
 
 pkgs.mkShell {
   buildInputs = [
     myJulia
-    pkgs.python3
-    pkgs.python3Packages.numpy
   ];
 
   JULIA_DEPOT_PATH="/Users/dom/.julia";
+  MYVARIABLE = "hello";
 }
--- simple.jl
+++ simple.jl
@@ -9,7 +9,8 @@ using LinearAlgebra
 # FIXME: This doesn't seem to give reproducibility
 rng = MersenneTwister(1234);
 
-T = 500;
+# T = 500;
+T = 50;
 
 deltaT = 0.01;
 g  = 9.81;
@@ -137,7 +138,8 @@ function pf(inits, N, f, h, y, Q, R, nx)
 
 end
 
-N = 50;
+# N = 50;
+N = 49;
 
 inits = zeros(nx, N);
 inits[1, :] .= 0.01;
@@ -147,13 +149,13 @@ inits[2, :] .= 0.00;
 
 plot(layer(y = x[1:T,1], Geom.line, Theme(default_color=color("red"))), layer(y = map(x -> x / 50, sum(foo[1,:,:],dims=1)), Geom.line))
 
-
 function pmh(inits, K, N, n_th, y, f_g, g, nx, prior_sample, prior_pdf, Q, R)
 
-    theta = zeros(n_th ,K+1);
+    T = length(y);
+    theta = zeros(n_th, K+1);
     log_W = -Inf;
     # FIXME:
-    x_pfs = zeros(nx,N,T, K);
+    x_pfs = zeros(nx, N, T, K);
 
     while log_W == -Inf # Find an initial sample without numerical problems
         theta[:, 1] = 9 .+ prior_sample(1);
@@ -169,7 +171,7 @@ function pmh(inits, K, N, n_th, y, f_g, g, nx, prior_sample, prior_pdf, Q, R)
         x_pfs[:, :, :, k] = a;
         mh_ratio = exp(log_W_prop - log_W) * prior_pdf(theta_prop) / prior_pdf(theta[:,k]);
 
-        # display([theta[:, k], theta_prop, log_W, log_W_prop, mh_ratio]);
+        display([theta[:, k], theta_prop, log_W, log_W_prop, mh_ratio, prior_pdf(theta_prop)]);
 
         if isnan(mh_ratio)
             alpha = 0;
@@ -196,7 +198,111 @@ function pmh(inits, K, N, n_th, y, f_g, g, nx, prior_sample, prior_pdf, Q, R)
     return (x_pfs, theta);
 end
 
+# (fuu, fee) = pmh(inits, K[1], N, n_th, y, f_g, h, nx, prior_sample, prior_pdf, bigQ, bigR)
+(fuu, fee) = pmh(inits, K[1], N, n_th, y, f_g, h, nx, prior_sample, prior_pdf, bigQ, bigR)
+
 plot(layer(y = x[1:T,1], Geom.line, Theme(default_color=color("red"))), layer(y = map(x -> x / 50, sum(fuu[1,:,:,1],dims=1)), Geom.line), layer(y = map(x -> x / 50, sum(fuu[1,:,:,50],dims=1)), Geom.line, Theme(default_color=color("green"))))
+
+function smc2(inits, Km, N_th, n_th, y, f_g, g, nx, prior_sample, prior_pdf, Q, R)
+
+    T    = length(y);
+    x_th = zeros(n_th, N_th, T+1);
+
+    x_th[:, :, 1] = prior_sample(N_th);
+
+    N_x = zeros(T, 1);
+    N_x = map(x -> max(30, x), 1:T);
+
+    N     = maximum(N_x);
+    log_w = zeros(T, N, N_th);
+    x_pf  = zeros(nx, N, T, N_th);
+    w_th  = zeros(N_th,T);
+
+    for t = 1:T
+
+        for m = 1:N_th
+            if t >= 2
+                wn = zeros(1:N_x[t-1]);
+                wn[1:N_x[t-1]] = map(exp, log_w[t-1, 1:N_x[t-1], m] .- maximum(log_w[t-1, 1:N_x[t-1], m]));
+                wn[1:N_x[t-1]] = wn[1:N_x[t-1]] / sum(wn[1:N_x[t-1]]);
+                a = resample_stratified(wn);
+                x_pf[:, 1:N_x[t], t, m) = f_g(x_pf[:, a, t-1, m], x_th[:, m, t]) + sqrt(Q)*randn(nx,N_x(t));
+            end
+            log_w[t, 1:N_x[t], m] = logpdf(MvNormal(y[t, :], R), h(x_pf[:, 1:N_x[t], t, m]));
+        end
+
+        if t >= 2
+            # w_incr = squeeze(sum(1/N_x(t)*exp(log_w(t,1:N_x(t),:)),2));
+            # w_th(:,t) = w_th(:,t-1).*w_incr;
+            # cum_log_w = cum_log_w + log(w_incr);
+        else
+            w_th[:, 1] = dropdims(sum(1 / N_x[t] * map(exp, log_w[t,1:N_x[t],:]), dims = 1), dims = 1);
+            cum_log_w = map(log, w_th[:, 1]);
+        end
+
+        w_th[:, t] = w_th[:, t] ./ sum(w_th[:, t]);
+        ess = (sum(w_th[:, t]))^2 / sum(w_th[:, t].^2);
+
+#     if ESS<0.3*N_th
+
+#         a = systematic_resampling(w_th(:,t),N_th);
+#         x_pf(:,1:N_x(t),t,:) = x_pf(:,1:N_x(t),t,a);
+#         log_w(t,1:N_x(t),:) = log_w(t,1:N_x(t),a);
+#         x_th(:,:,t) = x_th(:,a,t);
+#         cum_log_w = cum_log_w(a);
+#         w_th(:,t) = 1/N_th;
+
+#         for k = 1:Km
+
+#             for m = 1:N_th
+
+#                 if k == 1
+#                     x_th_prop = x_th(:,m,t) + 0.1*randn(n_th,1);
+#                 else
+#                     x_th_prop = x_th(:,m,t+1) + 0.1*randn(n_th,1);
+#                 end
+
+#                 [log_W_prop, x_pf_prop, log_w_prop] = pf( N_x(t), @(x,u)f_g(x,u,x_th_prop), g, u(1:t), y(1:t,:), Q, R, nx);
+
+# %                 cum_log_w(m) = pf( N_x(t), @(x,u)f_g(x,u,x_th(:,m,t)), g, u(1:t), y(1:t,:), Q, R, nx);
+
+#                 dm = rand;
+#                 mh_ratio = exp(log_W_prop-cum_log_w(m))*prior_pdf(x_th_prop)/prior_pdf(x_th(:,m,t));
+#                 if isnan(mh_ratio)
+#                     alpha = 0;
+#                 else
+#                     alpha = min(1,mh_ratio);
+#                 end
+#                 if dm < alpha
+#                     x_th(:,m,t+1) = x_th_prop;
+#                     cum_log_w(m) = log_W_prop;
+#                     x_pf(:,1:N_x(t),t,m) = x_pf_prop;
+#                     log_w(t,1:N_x(t),m) = log_w_prop;
+# %                     disp('a!')
+#                 else
+#                     if k == 1
+#                         x_th(:,m,t+1) = x_th(:,m,t);
+#                     else
+#                         x_th(:,m,t+1) = x_th(:,m,t+1);
+#                     end
+# %                     disp('r')
+#                 end
+#             end
+#             display(['Propagating with PMH. Step ', num2str(k), '/', num2str(Km)])
+#         end
+
+#     else
+#         x_th(:,:,t+1) = x_th(:,:,t);
+#     end
+
+        clf
+        scatter(t*ones(N_th,1),x_th(1,:,t),w_th(:,t)*10000+1,'r','filled')
+        xlim([1 T])
+        drawnow
+        display(['SMC^2, t = ', num2str(t)])
+    end
+end
+
 
 
 # for i = 1:3
